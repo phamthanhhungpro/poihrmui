bcc936c9fea66d3f682f1e259cd70823435bdcae
Hung Pham Thanh <hung.phamthanh@nashtechglobal.com>
2024-06-28 16:55:22 +0700

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,28 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector   : 'app-root',
+    selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls  : ['./app.component.scss'],
-    standalone : true,
-    imports    : [RouterOutlet],
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [RouterOutlet],
 })
-export class AppComponent
-{
+export class AppComponent {
     /**
      * Constructor
      */
-    constructor()
-    {
+    constructor() {
+    }
+    ngOnInit() {
+        window.addEventListener('storage', (event) => {
+            if (event.key === 'logout-event') {
+                localStorage.removeItem('accessToken');
+                localStorage.removeItem('expireDate');
+                localStorage.removeItem('refreshToken');
+                localStorage.removeItem('role');
+                localStorage.removeItem('tenantId');
+                localStorage.removeItem('userId');
+            }
+        }, false);
     }
 }
